7b80b6f9b6ca346b41f14f3c7dee2ae79323efaf
MDeyanov <justdeyanov@icloud.com>
2023-04-03 01:52:31 +0300

    feat:edit and cloudinary

--- src/components/CreateProduct/CreateProduct.js
+++ src/components/CreateProduct/CreateProduct.js
@@ -1,18 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useForm } from "../../hooks/useForm";
-import {Form, Button} from 'react-bootstrap';
+import { Form, Button } from 'react-bootstrap';
 import { usePaintingContext } from '../../contexts/PaintingContext';
 
 
-export const CreateProduct = () => {   
+export const CreateProduct = () => {
+    const [imageUrl, setImageUrl] = useState("");
     const { onCreatePaintingSubmit } = usePaintingContext();
     const { values, changeHandler, onSubmit } = useForm({
         title: '',
         size: '',
-        price: '',
         imageUrl: '',
+        price: '',
         summary: '',
     }, onCreatePaintingSubmit);
+
+    const openWidget = () => {
+        const widget = window.cloudinary.createUploadWidget(
+            {
+                cloudName: "dzac3ggur",
+                uploadPreset: "react-course",
+            },
+            (error, result) => {
+                if (result.event === "success" && result.info.url !== undefined) {
+                    setImageUrl(result.info.url);
+                }
+            }
+        );
+
+        widget.open();
+    };
 
     return (
         <Form id="create" method="post" onSubmit={onSubmit}>
@@ -30,14 +47,21 @@ export const CreateProduct = () => {
             </Form.Group>
             <Form.Group className="mb-3">
                 <Form.Label>Image:</Form.Label>
-                <Form.Control value={values.imageUrl} onChange={changeHandler} type="text" id="imageUrl" name="imageUrl" placeholder="Upload a photo..." />
+                <Form.Control
+                    id="imageUrl"
+                    name="imageUrl"
+                    onChange={changeHandler}
+                    value={values.imageUrl = imageUrl}
+                    disabled
+                />
+                <Button variant="primary" onClick={openWidget} type="button">
+                    Upload Image
+                </Button>
             </Form.Group>
             <Form.Group className="mb-3">
                 <Form.Label>Summary:</Form.Label>
                 <Form.Control value={values.category} onChange={changeHandler} type="text" id="summary" name="summary" placeholder="Summary" />
             </Form.Group>
-
-
             <Button variant="primary" type="submit">
                 Create product
             </Button>
---
+++ src/components/EditPainting/EditPainting.js
@@ -0,0 +1,115 @@
+import React, { useEffect,useState } from "react";
+import { useParams } from "react-router-dom";
+import {Form, Button} from 'react-bootstrap';
+
+import { useForm } from "../../hooks/useForm";
+import { useService } from "../../hooks/useService";
+import { paintingServiceFactory } from "../../services/paintingService";
+
+import { usePaintingContext } from "../../contexts/PaintingContext";
+
+export const EditPainting = () => {
+    const [imageUrl, setImageUrl] = useState("");
+    const { onPaintingEditSubmit } = usePaintingContext();
+    const { paintingId } = useParams();
+    const paintingService = useService(paintingServiceFactory);
+
+    const { values, changeHandler, onSubmit, changeValues } = useForm({
+        _id: '',
+        title: '',
+        size: '',
+        price: '',
+        imageUrl: '',
+        summary: '',
+    }, onPaintingEditSubmit);
+
+    const openWidget = () => {
+        const widget = window.cloudinary.createUploadWidget(
+            {
+                cloudName: "dzac3ggur",
+                uploadPreset: "react-course",
+            },
+            (error, result) => {
+                if (result.event === "success" && result.info.url !== undefined) {
+                    setImageUrl(result.info.url);
+                }
+            }
+        );
+
+        widget.open();
+    };
+
+    useEffect(() => {
+        paintingService.getOne(paintingId)
+            .then(result => {
+                changeValues(result);
+            });
+    }, [paintingId]);
+
+    return (
+        <Form id="edit" method="post" onSubmit={onSubmit}>
+            <Form.Group className="mb-3">
+                <Form.Label>Product Title:</Form.Label>
+                <Form.Control
+                    value={values.title}
+                    onChange={changeHandler}
+                    type="text"
+                    id="title"
+                    name="title"
+                    placeholder="Product Title"
+                />
+            </Form.Group>
+            <Form.Group className="mb-3">
+                <Form.Label>Size:</Form.Label>
+                <Form.Control
+                    value={values.size}
+                    onChange={changeHandler}
+                    type="text"
+                    id="size"
+                    name="size"
+                    placeholder="Size"
+                />
+            </Form.Group>
+            <Form.Group className="mb-3">
+                <Form.Label>Price:</Form.Label>
+                <Form.Control
+                    value={values.price}
+                    onChange={changeHandler}
+                    type="text"
+                    id="price"
+                    name="price"
+                    placeholder="Price"
+                />
+            </Form.Group>
+            <Form.Group className="mb-3">
+            <Form.Label>Image:</Form.Label>
+                <Form.Control
+                    id="imageUrl"
+                    name="imageUrl"
+                    onChange={changeHandler}
+                    value={values.imageUrl = imageUrl}
+                    disabled
+                />
+                <Button variant="primary" onClick={openWidget} type="button">
+                    Upload Image
+                </Button>
+            </Form.Group>
+            <Form.Group className="mb-3">
+                <Form.Label>Summary:</Form.Label>
+                <Form.Control
+                    value={values.category}
+                    onChange={changeHandler}
+                    type="text"
+                    id="summary"
+                    name="summary"
+                    placeholder="Summary"
+                />
+            </Form.Group>
+
+
+            <Button variant="primary" type="submit">
+                Edit Product
+            </Button>
+        </Form>
+    );
+}
